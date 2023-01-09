16678ff3f595e6d37b884a826e178a2bde152c9a
ankitverma <ankit.verma@affle.com>
2023-01-09 11:46:28 +0530

    update

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -813,10 +813,21 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-$databases['default']['default'] = array (
+/*$databases['default']['default'] = array (
   'database' => 'thebar',
   'username' => 'root',
   'password' => 'admin',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'driver' => 'mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);*/
+$databases['default']['default'] = array (
+  'database' => 'ankitver_thebar',
+  'username' => 'ankitver_drupal',
+  'password' => '8y{a(o-IrhQ~',
   'prefix' => '',
   'host' => 'localhost',
   'port' => '3306',
